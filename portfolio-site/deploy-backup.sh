b25c52fa93f715ef064367f21c5ce1bcdf0ccf11
Antonios Gkamitsios <angam97@gmail.com>
2023-10-27 12:11:05 +0000

    Change deploy backup script

--- portfolio-site/deploy-backup.sh
+++ portfolio-site/deploy-backup.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-docker-compose -f docker-compose.backup.yaml up -d --build --wait --scale node-container-backup=2 &&
+cd /var/www/myportfolio/portfolio-site/
+docker-compose -f docker-compose.backup.yaml up -d --build --scale node-container-backup=2 &&
     docker system prune -f
