f1827ddb0c88fdc23096dc6b882bf3eb53c4137a
Antonios Gkamitsios <angam97@gmail.com>
2023-10-27 12:14:06 +0000

    Add git pull command in backup deploy script

--- portfolio-site/deploy-backup.sh
+++ portfolio-site/deploy-backup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 cd /var/www/myportfolio/portfolio-site/
+git pull origin main --ff-only
 docker-compose -f docker-compose.backup.yaml up -d --build --scale node-container-backup=2 &&
     docker system prune -f
